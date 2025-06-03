1f68412c4d3f12b4d2ab7875ecd87afa37c84603
RahimiAli <mohdrahimi.mohdali@credence.tech>
2025-06-03 16:44:25 +0800

    fix: dada

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -1,6 +1,7 @@
-import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity, SafeAreaView } from 'react-native';
 import { EditScreenInfo } from './EditScreenInfo';
 import { Ionicons } from '@expo/vector-icons';
+import { Container } from './Container';
 
 type ScreenContentProps = {
   title: string;
@@ -18,11 +19,8 @@ export const ScreenContent = ({
   children,
 }: ScreenContentProps) => {
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      className="bg-white"
-      showsVerticalScrollIndicator={false}>
-      <View className={styles.container}>
+    <>
+      <SafeAreaView className="h-[20rem] rounded-b-[3rem] bg-[#42e3f5] bg-blend-color-dodge">
         <View className="mb-2 w-full flex-row items-center justify-center">
           {showBackButton && (
             <TouchableOpacity onPress={onBackPress} className="absolute left-0 p-2">
@@ -31,17 +29,25 @@ export const ScreenContent = ({
           )}
           <Text className={styles.title}>{title}</Text>
         </View>
-
-        <View className={styles.separator} />
-        <EditScreenInfo path={path} />
-        {children}
-      </View>
-    </ScrollView>
+      </SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        className="bg-transparent"
+        showsVerticalScrollIndicator={false}>
+        <View className={styles.container}>
+          {/* <View className={styles.separator} /> */}
+          {/* <EditScreenInfo path={path} /> */}
+          {/* <Header>{children}</Header> */}
+          <Container>{children}</Container>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
 const styles = {
   container: `items-center flex-1 justify-start pt-6 px-4 pb-20`,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-2xl font-bold text-blue-600`,
+  title: `text-2xl font-bold text-blue-600 subpixel-antialiased
+`,
 };
